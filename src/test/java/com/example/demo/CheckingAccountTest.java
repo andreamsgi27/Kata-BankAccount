4b49ca7579e33681892b0b5baf97a684c1f175c6
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-28 23:39:26 +0100

    "Updated CheckingAccountTest class with new test methods and modified existing ones. 84% total coverage"

--- src/test/java/com/example/demo/CheckingAccountTest.java
+++ src/test/java/com/example/demo/CheckingAccountTest.java
@@ -1,25 +1,47 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 public class CheckingAccountTest {
     @Test
-    void testApplyMonthlyFee() {
-
-    }
-
-    @Test
     void testMakeDeposit() {
-
-    }
-
-    @Test
-    void testPrintAccount() {
-
+        CheckingAccount account = new CheckingAccount(1000f, 10f, -50f);
+        account.makeDeposit(100f);
+        assertThat(account.balance).isEqualTo(1100f);
     }
 
     @Test
     void testWithdraw() {
+        CheckingAccount account = new CheckingAccount(1000f, 10f, -50f);
+        account.withdraw(100f);
+        assertThat(account.balance).isEqualTo(900f);
+    }
 
+    @Test
+    void testWithdrawExceedsLimit() {
+        CheckingAccount account = new CheckingAccount(1000f, 10f, -50f);
+        account.withdraw(10000f);
+        assertThat(account.balance).isEqualTo(1000f);
+    }
+
+    @Test
+    void testApplyMonthlyFee() {
+        CheckingAccount account = new CheckingAccount(10000f, 10f, -50f);
+        account.withdraw(500f);
+        account.withdraw(500f);
+        account.withdraw(500f);
+        account.withdraw(500f);
+        account.withdraw(500f);
+        account.applyMonthlyFee();
+        assertThat(account.balance).isEqualTo(6500.0f);
+    }
+
+    @Test
+    void testPrintAccount() {
+        BankAccount account = new CheckingAccount(1000f, 0.05f, -50f);
+        String expected = "Balance: 1000,00, Deposits: 0, Withdrawals: 0, Annual Rate: 0,05%, Overdraft Limit: -50,00";
+        String actual = account.printAccount();
+        assertThat(actual).isEqualTo(expected);
     }
 }
